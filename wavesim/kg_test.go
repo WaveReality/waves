58250a13cfbc74fe8764d863c8393e089047b15d
Randall C. O'Reilly <oreilly@ucdavis.edu>
2026-09-22 22:46:39 +0200

    minor test fix

--- wavesim/kg_test.go
+++ wavesim/kg_test.go
@@ -195,7 +195,7 @@ func kgStepExt(ss *Sim) {
 	ctx := GetCtx(0)
 	ctx.StepInc()
 	RunKleinGordonCKernel(int(ctx.Size.X * ctx.Size.Y * ctx.Size.Z))
-	if ss.Params.Edges == EdgeWrap {
+	if ss.Params.Edges == EdgesWrap {
 		RunEdgesWrapKernel(int(ctx.EdgesN()))
 	}
 	ss.RunStats(false)
